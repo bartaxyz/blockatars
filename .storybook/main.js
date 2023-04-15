0362045e49f0a9b9ef2d1a29ab98d17a44b8e60f
Ondřej Bárta <email@ondrejbarta.xyz>
2023-04-15 17:09:08 +0200

    chore: update all packages to latest versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
